b4a8c241b1a1bc23a66cb943033d4ea46737e090
Leonardo Vargas <leonardovargasdpaula@gmail.com>
2025-09-02 09:20:29 -0400

    feat: adiciona agendamento para as 4:35 AM

--- dags/main_pipeline.py
+++ dags/main_pipeline.py
@@ -16,7 +16,7 @@ TABLES_BANVIC = ["agencias", "clientes",
 @dag(
     dag_id='processamento_dados_banvic',
     start_date=datetime(2025, 9, 1),
-    schedule=None,
+    schedule="35 4 * * *",
     catchup=False,
     tags=['pandas', 'sql', 'etl'],
     doc_md="""
